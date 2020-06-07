9463de13050b2d07c8162cfee339d1691e91edaf
Jordo <jordanrbunnell@gmail.com>
2020-06-06 21:31:47 -0600

    Added script to limit to number input only and 128 max limit, also added 'Insane' character option

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 // Assignment Code
+
+// Restricts input for the given textbox to the given inputFilter. Thanks to: https://jsfiddle.net/emkey08/zgvtjc51
+function setInputFilter(textbox, inputFilter) {
+  ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function(event) {
+    textbox.addEventListener(event, function() {
+      if (inputFilter(this.value)) {
+        this.oldValue = this.value;
+        this.oldSelectionStart = this.selectionStart;
+        this.oldSelectionEnd = this.selectionEnd;
+      } else if (this.hasOwnProperty("oldValue")) {
+        this.value = this.oldValue;
+        this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+      } else {
+        this.value = "";
+      }
+    });
+  });
+}
+
+
+// Add filter to the "length" element to only allow for numerical values for character length <= 128.
+setInputFilter(document.getElementById("length"), function(value) {
+  return /^\d*$/.test(value) && (value === "" || parseInt(value) <= 128); });
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 var lowercase = "abcdefghijklmnopqrstuvwxyz",
   uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
   numbers = "0123456789",
   punctuation = "!@#$%^&*()_+~`|}{[]:;?><,./-=",
+  wild = "☝☥☦☙☆⚉☜☁☂☄☞♲☬☤☍⚾♨★☸⚔✍✈☣♽⚜☠☢☘"
   lowercaseInput = document.getElementById("lowercase"),
   uppercaseInput = document.getElementById("uppercase"),
   punctuationInput = document.getElementById("punctuation"),
@@ -13,9 +40,9 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz",
   passwordFeild = document.getElementById("password"),
   generateButton = document.getElementById("generate"),
   copyButton = document.getElementById("copy"),
-  alertbox = document.getElementById('alert');
-  
-
+  alertbox = document.getElementById('alert'),
+  wildInput = document.getElementById("wild");
+//This section generates the string based on the user input of desired character sets.
   function generate() {
     userPassword = "";
     passOptions = "";
@@ -30,6 +57,9 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz",
     }
     if (numbersInput.checked) {
       passOptions += numbers;
+    }
+    if (wildInput.checked) {
+      passOptions += wild;
     }
 // This ensures the input is a number, if it is Not then a NuN is returned
     plength = Number(lengthInput.value);
@@ -47,13 +77,9 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz",
     console.log(notANumber);
 
     
-    // if (plength > 128 || plength < 1) {
-    //   alertbox.innerHTML = "Password must be between 8 and 128 characters"
-      
-    // } 
+   
     if (notANumber) {
       
-      // alert ("Please Select Character Type Before Generating");
       alert("You must enter a number between 8 and 128");
 
     } 
@@ -64,31 +90,21 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz",
     } 
     else if (userPassword == "") {
       
-      // alert ("Please Select Character Type Before Generating");
       alertbox.innerHTML = "Please Choose a Character Type"
 
     } 
     
     else {
+      // clears alert on successful run
       alertbox.innerHTML = ""
       
-
+      //Writes string "userPassword" to "password" Element ID in html
       passwordFeild.innerHTML = userPassword;
     }
     
   };
-  generateButton.addEventListener("click", generate);
    
   
 
-// Write password to the #password input
-function writePassword() {
-  var password = userpassword("");
-  var passwordText = document.querySelector("#generate");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("generate", writePassword);
+// // Add event listener to generate button
+generateButton.addEventListener("click", generate);
